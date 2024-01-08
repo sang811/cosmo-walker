a178fbaaaed9c9ea4238e545f09f3a5295c21f71
phung-tan-sang <phung_tan_sang@ca-adv.co.jp>
2024-01-08 10:30:06 +0700

    project initial comiit

---
+++ cosmo-walker/src/main/java/com/example/cosmowalker/CosmoWalkerApplication.java
@@ -0,0 +1,13 @@
+package com.example.cosmowalker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmoWalkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmoWalkerApplication.class, args);
+	}
+
+}
---
+++ cosmo-walker/src/test/java/com/example/cosmowalker/CosmoWalkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cosmowalker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmoWalkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
